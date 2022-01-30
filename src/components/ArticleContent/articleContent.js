4b1c4dfa4e293831bbb74c9354c382ae80d761db
Marydez <chukwumamary1@gmail.com>
2022-01-30 13:06:53 +0000

    Articles component added

--- src/components/ArticleContent/articleContent.js
+++ src/components/ArticleContent/articleContent.js
@@ -1,7 +1,17 @@
 import React from 'react';
+import { Button } from 'antd';
 
-const ArticleContent = () => {
-  return <div></div>;
+const ArticleContent = ({author:title, text}) => {
+  return (
+  <section className='section'>
+  <h2 className='title'>{title}</h2>
+  {text.map((p) => (
+      <p className='text' key ={p.id} >{p.text}</p>
+  ))}
+  <Button className='btn'  type="primary">Like 👍</Button>
+
+  </section>
+  )
 };
 
 export default ArticleContent;
